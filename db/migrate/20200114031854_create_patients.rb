70db47d7c904a8172d600f9f053e88620a9fdcdb
Emery Tate <tateejt@gmail.com>
2020-01-14 03:20:29 +0000

    Automatically backed up by Learn

--- db/migrate/20200114031854_create_patients.rb
+++ db/migrate/20200114031854_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
       t.string :name 
-      t.datetime :created_at
+      t.datetime :created_at, null: false
       t.integer :age 
     end
   end
